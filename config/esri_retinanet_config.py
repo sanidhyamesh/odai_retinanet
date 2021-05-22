ed34111fea60f15bee60f3095e61000eae844dee
Sanidhya <54096594+sanidhyamesh@users.noreply.github.com>
2021-05-22 20:28:02 +0530

    Update esri_retinanet_config.py

--- config/esri_retinanet_config.py
+++ config/esri_retinanet_config.py
@@ -2,14 +2,14 @@
 import os
 
 # initialize the base path for the logos dataset
-BASE_PATH = "dataset"
+BASE_PATH = "/content/odai_retinanet/dataset"
 
 # build the path to the annotations and input images
 ANNOT_PATH = os.path.sep.join([BASE_PATH, 'annotations'])
 IMAGES_PATH = os.path.sep.join([BASE_PATH, 'images'])
 
 # degine the training/testing split
-TRAIN_TEST_SPLIT = 1.00
+TRAIN_TEST_SPLIT = 0.80
 
 #  build the path to the output training and test .csv files
 TRAIN_CSV = os.path.sep.join([BASE_PATH, 'train.csv'])
